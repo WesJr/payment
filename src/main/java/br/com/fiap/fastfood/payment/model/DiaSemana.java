fa8fe1384060b310935aae3886406157e53756a3
Wesley Junior de Souza <wesjuniorsouza@gmail.com>
2025-07-02 19:56:28 -0300

    inclusao modelos loja

---
+++ src/main/java/br/com/fiap/fastfood/payment/model/DiaSemana.java
@@ -0,0 +1,11 @@
+package br.com.fiap.fastfood.payment.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DiaSemana {
+
+
+}
---
+++ src/main/java/br/com/fiap/fastfood/payment/model/Loja.java
@@ -0,0 +1,31 @@
+package br.com.fiap.fastfood.payment.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "loja")
+public class Loja {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "nome")
+    private String name;
+
+    @Column(name = "data_criacao")
+    private String dateCreated;
+
+    @Column(name = "id_externo")
+    private String externalId;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "address_id", referencedColumnName = "id")
+    private Endereco endereco;
+
+
+}
